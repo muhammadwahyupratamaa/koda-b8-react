dc314687312b30bb22f18306090e4347ff44b7e5
Muhammad Wahyu Pratama <muhammad123wahyu123@gmail.com>
2026-08-10 02:00:09 +0700

    feat: add wishlist reducer to the store configuration

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import cartReducer from "../features/cart/cartSlice";
+import wishlistReducer from "../features/wishlist/wishlistSlice";
 
 const rootReducer = combineReducers({
   cart: cartReducer,
+  wishlist: wishlistReducer,
 });
 
 export const store = configureStore({
