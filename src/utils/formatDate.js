7d1dc05dd8c176ca51645f454c9fb4ba45e9125b
Muhammad Wahyu Pratama <muhammad123wahyu123@gmail.com>
2026-08-15 22:32:36 +0700

    feat: implement formatDate utility for consistent date formatting

---
+++ src/utils/formatDate.js
@@ -0,0 +1,9 @@
+function formatDate(date) {
+  return new Intl.DateTimeFormat("id-ID", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  }).format(new Date(date));
+}
+
+export default formatDate;
